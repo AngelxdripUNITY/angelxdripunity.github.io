94c98e020ffe19035568c81c440ed1ed8b0f75eb
Angel <126852294+AngelxdripUNITY@users.noreply.github.com>
2025-02-25 08:16:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+document.getElementById("clickButton").addEventListener("click", function() {
+    alert("Button clicked!");
+});
+
+document.getElementById("up").addEventListener("click", function() {
+    alert("Up button pressed");
+});
+
+document.getElementById("down").addEventListener("click", function() {
+    alert("Down button pressed");
+});
+
+document.getElementById("left").addEventListener("click", function() {
+    alert("Left button pressed");
+});
+
+document.getElementById("right").addEventListener("click", function() {
+    alert("Right button pressed");
+});
